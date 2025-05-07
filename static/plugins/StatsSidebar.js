1458be32d4b852f34ead0625424eefa51ad58040
7r1UMPH <Triumph202402@gmail.com>
2025-05-07 08:34:04 +0800

    优化4

--- static/plugins/StatsSidebar.js
+++ static/plugins/StatsSidebar.js
@@ -12,7 +12,7 @@ function createStatsSidebar() {
     sidebar.className = 'stats-sidebar';
     
     // 博客头像URL
-    const avatarUrl = 'https://cdn.jsdelivr.net/gh/7r1UMPH/7r1UMPH.github.io@main/static/image/20250506213832365.webp';
+    const avatarUrl = 'https://hub.gitmirror.com/https://raw.githubusercontent.com/7r1UMPH/7r1UMPH.github.io/main/static/image/20250506213832365.webp';
     
     const isArticlePage = window.location.pathname.includes('/post/');
     
