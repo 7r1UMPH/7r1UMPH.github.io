a3779698eec2af34ce0afde6a771dc2d6e2fc499
7r1UMPH <null>
2025-05-19 04:33:06 +0000

    🎉auto update by Gmeek action

--- docs/plugins/StatsSidebar.js
+++ docs/plugins/StatsSidebar.js
@@ -2,23 +2,6 @@ function runStatsSidebar() {
     if (window.innerWidth < 768) {
         return;
     }
-    // 预加载字体
-    const preloadFonts = document.createElement('link');
-    preloadFonts.rel = 'preload';
-    preloadFonts.href = 'https://7r1umph.top/Font/fa-regular-400.woff2';
-    preloadFonts.as = 'font';
-    preloadFonts.type = 'font/woff2';
-    preloadFonts.crossOrigin = 'anonymous';
-    document.head.appendChild(preloadFonts);
-
-    const preloadFonts2 = document.createElement('link');
-    preloadFonts2.rel = 'preload';
-    preloadFonts2.href = 'https://7r1umph.top/Font/fa-solid-900.woff2';
-    preloadFonts2.as = 'font';
-    preloadFonts2.type = 'font/woff2';
-    preloadFonts2.crossOrigin = 'anonymous';
-    document.head.appendChild(preloadFonts2);
-    
     createStatsSidebar();
     
     // 加载vercount统计脚本
